b40873acc2531d3544026c7b1bb58cad78521682
nagai <c1248@oic.jp>
2023-10-12 13:40:26 +0900

    開発者用画面に画面遷移用のボタンを実装

--- GameSoftSaleSystems/開発用画面移動画面.cs
+++ GameSoftSaleSystems/開発用画面移動画面.cs
@@ -33,6 +33,21 @@ namespace GameSoftSaleSystems
                 case "FormSubscription":
                     frm = new FormSubscription();
                     break;
+                case "FormTitleDetail":
+                    frm = new FormTitleDetail();
+                    break;
+                case "FormTitleDetailRegist":
+                    frm = new FormTitleDetailRegist();
+                    break;
+                case "FormTitleDetailUpdate":
+                    frm = new FormTitleDetailUpdate();
+                    break;
+                case "FormViewAccounts":
+                    frm = new FormViewAccounts();
+                    break;
+                case "FormInput":
+                    frm = new FormInput();
+                    break;
             }
 
             //フォームを透明化
@@ -43,6 +58,7 @@ namespace GameSoftSaleSystems
 
             //開いたフォームから戻ってきたらメモリを解放する
             frm.Dispose();
+            Opacity = 1;
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -57,6 +73,74 @@ namespace GameSoftSaleSystems
         private void button3_Click(object sender, EventArgs e)
         {
             OpenForm("FormSubscription");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            FontDialog fd = new FontDialog();
+
+            //初期のフォントを設定
+            fd.Font = label2.Font;
+            //初期の色を設定
+            fd.Color = label2.ForeColor;
+            //ユーザーが選択できるポイントサイズの最大値を設定する
+            fd.MaxSize = 15;
+            fd.MinSize = 10;
+            //存在しないフォントやスタイルをユーザーが選択すると
+            //エラーメッセージを表示する
+            fd.FontMustExist = true;
+            //横書きフォントだけを表示する
+            fd.AllowVerticalFonts = false;
+            //色を選択できるようにする
+            fd.ShowColor = true;
+            //取り消し線、下線、テキストの色などのオプションを指定可能にする
+            //デフォルトがTrueのため必要はない
+            fd.ShowEffects = true;
+            //固定ピッチフォント以外も表示する
+            //デフォルトがFalseのため必要はない
+            fd.FixedPitchOnly = false;
+            //ベクタ フォントを選択できるようにする
+            //デフォルトがTrueのため必要はない
+            fd.AllowVectorFonts = true;
+
+            //ダイアログを表示する
+            if (fd.ShowDialog() != DialogResult.Cancel)
+            {
+                //TextBox1のフォントと色を変える
+                label2.Font = fd.Font;
+                label2.ForeColor = fd.Color;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenForm("FormTitleDetail");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenForm("FormTitleDetailRegist");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            OpenForm("FormTitleDetailUpdate");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            OpenForm("FormViewAccounts");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            OpenForm("FormInput");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //DialogResult=new DialogResult
         }
     }
 }
